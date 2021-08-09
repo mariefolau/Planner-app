62029ba843e634cd92dadcbf60a0e0e5680ff69c
mariefolau <81399551+mariefolau@users.noreply.github.com>
2021-08-09 21:11:38 +1000

    Added switch statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.glass.ui.Clipboard;
+import com.sun.org.apache.bcel.internal.generic.Select;
 import jdk.internal.util.xml.impl.Input;
 
 import java.time.LocalDate;
@@ -8,6 +10,8 @@ import java.time.LocalTime;
 import java.util.*;
 
 public class Main {
+
+    private static Clipboard toDoList;
 
     public static void main(String[] args) {
 
@@ -26,16 +30,57 @@ public class Main {
         Scanner list = new Scanner(System.in);
 
         String line;
-        while(!(line = input.nextLine()).equals("end")){
+        while (!(line = input.nextLine()).equalsIgnoreCase("end")) {
             toDoList.add(line);
         }
 
         Collections.sort(toDoList);
         System.out.println(toDoList);
+
+        int Selected;
+        do {
+            Selected = yesOrNo();
+            switch (Selected) {
+                case 1:
+                    System.out.println("Option 1 selected. Please add to your task");
+                    break;
+                case 2:
+                    System.out.println("Please continue ahead.");
+                    break;
+                default:
+                    break;
+            }
+        }
+        while (Selected > 2);
     }
 
+    public static int yesOrNo() {
+        int Selection;
+        Scanner yn = new Scanner(System.in);
+        System.out.println("Do you want to add to your task?");
+        System.out.println("1 - yes");
+        System.out.println("2 - no");
 
-//        TimerTask task = new TimerTask() {
+        Selection = yn.nextInt();
+
+        if (Selection == 1) {
+            
+        } else {
+
+        }
+        return Selection;
+
+    }
+}
+
+
+
+
+
+
+
+
+       // TimerTask task = new TimerTask() {
 //            @Override
 //            public void run() {
 //                System.out.println("Task complete");
@@ -48,7 +93,7 @@ public class Main {
 //        timer.cancel();
 
 
-}
+
 
 
 
